81ca23e0565032a25d870e32225109210ba4a373
baiwanli <2700320093@qq.com>
2025-10-18 15:13:06 +0800

    🔄Update: 添加 @MapperScan 注解

--- src/main/java/cn/hezhaohui/thumb/Application.java
+++ src/main/java/cn/hezhaohui/thumb/Application.java
@@ -1,8 +1,10 @@
 package cn.hezhaohui.thumb;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("cn.hezhaohui.thumb.mapper")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
