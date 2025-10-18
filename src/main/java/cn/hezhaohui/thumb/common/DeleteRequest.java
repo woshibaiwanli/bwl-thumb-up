62701cbf4539ce42e4bc70c43543d6e4fc01e98a
baiwanli <2700320093@qq.com>
2025-10-18 14:40:24 +0800

    🔄Update: 新增请求包装类

---
+++ src/main/java/cn/hezhaohui/thumb/common/DeleteRequest.java
@@ -0,0 +1,13 @@
+package cn.hezhaohui.thumb.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class DeleteRequest implements Serializable {
+
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
