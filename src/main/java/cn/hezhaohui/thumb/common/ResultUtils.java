38bbcdfdfd2c27bad5a9ee8376a95e2d1a3798b5
baiwanli <2700320093@qq.com>
2025-10-18 14:30:40 +0800

    🔄Update: 增加响应包装类

---
+++ src/main/java/cn/hezhaohui/thumb/common/ResultUtils.java
@@ -0,0 +1,22 @@
+package cn.hezhaohui.thumb.common;
+
+import cn.hezhaohui.thumb.exception.ErrorCode;
+
+public class ResultUtils {
+
+    public static <T> BaseResponse<T> sucess(T data) {
+        return new BaseResponse<>(ErrorCode.SUCCESS.getCode(), data, ErrorCode.SUCCESS.getMessage());
+    }
+
+    public static BaseResponse<?> error(ErrorCode errorCode) {
+        return new BaseResponse<>(errorCode);
+    }
+
+    public static BaseResponse<?> error(int code, String message) {
+        return new BaseResponse<>(code, null, message);
+    }
+
+    public static BaseResponse<?> error(ErrorCode errorCode, String message) {
+        return new BaseResponse<>(errorCode.getCode(), null, message);
+    }
+}
