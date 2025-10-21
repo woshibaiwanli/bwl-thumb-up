79427e57c23d6c6d027b9efa3e30573aac25c851
baiwanli <2700320093@qq.com>
2025-10-21 19:18:50 +0800

    ♻️Refactor: 修改常量的定义方式

--- src/main/java/cn/hezhaohui/thumb/constant/ThumbConstant.java
+++ src/main/java/cn/hezhaohui/thumb/constant/ThumbConstant.java
@@ -5,17 +5,17 @@ package cn.hezhaohui.thumb.constant;
  * 点赞常量接口
  * 用于定义与点赞功能相关的常量
  */
-public interface ThumbConstant {
+public class ThumbConstant {
 
     /**
      * 用户点赞前缀常量
      * 用于作为Redis中存储用户点赞信息的key前缀
      */
-    String USER_THUMB_KEY_PREFIX = "thumb:";
+    public static final String USER_THUMB_KEY_PREFIX = "thumb:";
 
     /**
      * 临时点赞前缀常量
      * 用于作为Redis中存储临时点赞信息的key前缀
      */
-    String TEMP_THUMB_KEY_PREFIX = "temp_thumb:%s";
+    public static final String TEMP_THUMB_KEY_PREFIX = "temp_thumb:%s";
 }
