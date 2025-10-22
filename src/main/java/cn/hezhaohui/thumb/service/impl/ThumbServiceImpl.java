7d748fc8d1e0199beebe839b7eb9226ea8328a0a
baiwanli <2700320093@qq.com>
2025-10-22 20:21:22 +0800

    🔄Update: 添加注解切换实际使用的 Bean

--- src/main/java/cn/hezhaohui/thumb/service/impl/ThumbServiceImpl.java
+++ src/main/java/cn/hezhaohui/thumb/service/impl/ThumbServiceImpl.java
@@ -23,7 +23,7 @@ import org.springframework.transaction.support.TransactionTemplate;
 * @description 针对表【thumb】的数据库操作Service实现
 * @createDate 2025-10-18 14:51:00
 */
-@Service
+@Service("thumbServiceDB")
 public class ThumbServiceImpl extends ServiceImpl<ThumbMapper, Thumb>
     implements ThumbService{
 
--- src/main/java/cn/hezhaohui/thumb/service/impl/ThumbServiceRedisImpl.java
+++ src/main/java/cn/hezhaohui/thumb/service/impl/ThumbServiceRedisImpl.java
@@ -29,7 +29,7 @@ import java.util.Arrays;
 * @description 针对表【thumb】的数据库操作Service实现
 * @createDate 2025-10-18 14:51:00
 */
-@Service
+@Service("thumbService")
 public class ThumbServiceRedisImpl extends ServiceImpl<ThumbMapper, Thumb>
     implements ThumbService{
 
