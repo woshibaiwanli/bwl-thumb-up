d7fce5bd4b7e54f0fe9f26f1e9e58b3d86ac2902
baiwanli <2700320093@qq.com>
2025-10-23 15:34:43 +0800

    ✨Feat: 通过补偿任务确保temp数据及时处理

--- src/main/java/cn/hezhaohui/thumb/Application.java
+++ src/main/java/cn/hezhaohui/thumb/Application.java
@@ -3,9 +3,11 @@ package cn.hezhaohui.thumb;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan("cn.hezhaohui.thumb.mapper")
 @SpringBootApplication
+@EnableScheduling
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
