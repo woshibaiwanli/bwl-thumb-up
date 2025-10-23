de1ea48cbe2e85bb03021e0e60d48032d49cbd75
baiwanli <2700320093@qq.com>
2025-10-23 14:49:37 +0800

    🔄Update: 自定义SQL批量更新博客点赞数

--- src/main/java/cn/hezhaohui/thumb/mapper/BlogMapper.java
+++ src/main/java/cn/hezhaohui/thumb/mapper/BlogMapper.java
@@ -2,6 +2,9 @@ package cn.hezhaohui.thumb.mapper;
 
 import cn.hezhaohui.thumb.model.entity.Blog;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
 
 /**
 * @author 23117
@@ -10,7 +13,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @Entity cn.hezhaohui.thumb.model.entity.Blog
 */
 public interface BlogMapper extends BaseMapper<Blog> {
-
+    void batchUpdateThumbCount(@Param("countMap") Map<Long, Long> countMap);
 }
 
 
